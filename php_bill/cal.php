d46f13ebd906a2093438118720fcea2e9500b3e9
thisisommore <ommore501@gmail.com>
2023-03-31 04:11:27 +0000

    Add UI to php calculator

--- php_bill/cal.php
+++ php_bill/cal.php
@@ -1,36 +1,68 @@
 <?php
-$units_used = 1000;
-$total_cost = 0;
-if ($units_used > 50) {
-    $total_cost += 3.50 * 50;
-    $units_used -= 50;
-} else {
-    $total_cost += 3.50 * $units_used;
-    echo $total_cost;
-    return;
+$total_cost = null;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["units_used"]) && !empty($_POST["units_used"])) {
+        $units_used = $_POST["units_used"];
+        $total_cost = 0;
+        if ($units_used > 50) {
+            $total_cost += 3.50 * 50;
+            $units_used -= 50;
+        } else {
+            $total_cost += 3.50 * $units_used;
+        }
+
+
+        if ($units_used > 100) {
+            $total_cost += 4 * 100;
+            $units_used -= 100;
+        } else {
+            $total_cost += 4 * $units_used;
+        }
+
+
+        if ($units_used > 100) {
+            $total_cost += 5.20 * 100;
+            $units_used -= 100;
+        } else {
+            $total_cost += 5.20 * $units_used;
+        }
+
+        $total_cost += 6.50 * $units_used;
+    } else {
+        $total_cost = null;
+    }
 }
 
-
-if ($units_used > 100) {
-    $total_cost += 4 * 100;
-    $units_used -= 100;
-} else {
-    $total_cost += 4 * $units_used;
-    echo $total_cost;
-    return;
-}
-
-
-if ($units_used > 100) {
-    $total_cost += 5.20 * 100;
-    $units_used -= 100;
-} else {
-    $total_cost += 5.20 * $units_used;
-    echo $total_cost;
-    return;
-}
-
-$total_cost += 6.50 * $units_used;
-echo "Bill is " . $total_cost . "\n";
-return;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="max.css">
+</head>
+
+
+<body>
+    <div class="center container">
+        <form action="" method="POST">
+            <input type="text" name="units_used" placeholder="Units used">
+            <button type="submit">Cal</button>
+            <h2>
+
+                <?php
+                if ($total_cost) {
+                    echo "Bill is " . $total_cost;
+                }
+                ?>
+            </h2>
+        </form>
+    </div>
+
+</body>
+
+</html>
